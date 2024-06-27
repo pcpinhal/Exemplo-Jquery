8ddfcf4c63da705fdb3185ac1648f90a62b847a3
pcpinhal <pc_pinhal@yahoo.com.br>
2024-06-27 11:59:21 -0300

    enimaçlão Jquery

--- script.js
+++ script.js
@@ -9,11 +9,9 @@ $(document).ready(function(){
     });
 
     $("#direita").click(function(){
-        $('.caixa').animate({right: '1000px'});        
-    });
-    $("#esquerda").click(function(){
         $('.caixa').animate({left: '1000px'});        
     });
-
-
+    $("#esquerda").click(function(){
+        $('.caixa').animate({left: '10px'});        
+    });
 });
